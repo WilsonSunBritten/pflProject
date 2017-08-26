dc9482ee6ccd1b5c4a84ceac3e7c22a205543e8b
Wilson Britten <wilsonsunbritten@gmail.com>
2017-08-26 13:32:58 -0600

    Build template json, minor other updates

---
+++ apiObjects.go
@@ -0,0 +1,49 @@
+package main
+
+type OrderCustomer struct {
+	FirstName   string `json:"firstName"`
+	LastName    string `json:"lastName"`
+	CompanyName string `json:"companyName"`
+	Address1    string `json:"address1"`
+	Address2    string `json:"address2"`
+	City        string `json:"City"`
+	State       string `json:"State"`
+	PostalCode  string `json:"postalCode"`
+	CountryCode string `json:"countryCode"`
+	Email       string `json:"email"`
+	Phone       string `json:"phone"`
+}
+
+type Item struct {
+	ItemSequenceNumber int            `json:"itemSequenceNumber"`
+	ProductID          int            `json:"productID"`
+	Quantity           int            `json:"quantity"`
+	TemplateData       []TemplateData `json:"templateData"`
+	ItemFile           string         `json:"itemFile"`
+}
+
+type TemplateData struct {
+	TemplateDataName  string `json:"templateDataName"`
+	TemplateDataValue string `json:"templateDataValue"`
+}
+
+type Shipment struct {
+	ShipmentSequenceNumber int    `json:"shipmentSequenceNumber"`
+	FirstName              string `json:"firstName"`
+	LastName               string `json:"lastName"`
+	CompanyName            string `json:"companyName"`
+	Address1               string `json:"address1"`
+	Address2               string `json:"address2"`
+	City                   string `json:"city"`
+	State                  string `json:"state"`
+	PostalCode             string `json"postalCode"`
+	CountryCode            string `json:"countryCode"`
+	Phone                  string `json:"phone"`
+	ShippingMethod         string `json:"shippingMethod"`
+}
+type CreateOrderObject struct {
+	PartnerOrderReference string        `json:"partnerOrderReference"`
+	OrderCustomer         OrderCustomer `json:"orderCustomer"`
+	Items                 []Item        `json:"items"`
+	Shipments             []Shipment    `json:"shipments"`
+}
--- main.go
+++ main.go
@@ -1,19 +1,26 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"github.com/tidwall/gjson"
-	//"html/template"
-	"bytes"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
+	"strconv"
 )
+
+const apiKey string = "136085"
+const apiUserName string = "miniproject"
+const apiPassword string = "Pr!nt123"
 
 func main() {
 	http.HandleFunc("/", defaultHandler)
 	http.HandleFunc("/showProductList", showProductListHandler)
 	http.HandleFunc("/fillInTemplatePage", fillInTemplatePageHandler)
+	http.HandleFunc("/processOrder", processOrderHandler)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
@@ -21,11 +28,67 @@ func main() {
 	}
 }
 
+func processOrderHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Method)
+	if r.Method == "POST" {
+		r.ParseForm()
+		fmt.Println(r.Form)
+		orderObject := getOrderObject(r.Form)
+		orderJson, err := json.Marshal(orderObject)
+		fmt.Println("orderObject json: " + string(orderJson))
+		fmt.Println("err: ", err)
+	}
+}
+
+func getOrderObject(formData url.Values) CreateOrderObject {
+	orderCustomer := OrderCustomer{
+		FirstName:   formData["firstName"][0],
+		LastName:    formData["lastName"][0],
+		CompanyName: formData["companyName"][0],
+		Address1:    formData["address1"][0],
+		Address2:    formData["address2"][0],
+		City:        formData["city"][0],
+		State:       formData["state"][0],
+		PostalCode:  formData["postalCode"][0],
+		CountryCode: formData["countryCode"][0],
+		Email:       formData["email"][0],
+		Phone:       formData["phone"][0]}
+	templateData := []TemplateData{}
+	//Todo ^ populate template data from form data(any field taht starts with __template... should const that...
+	prodID, _ := strconv.Atoi(formData["productID"][0])
+	prodQuantity, _ := strconv.Atoi(formData["quantity"][0])
+	item := Item{
+		ItemSequenceNumber: 1,
+		ProductID:          prodID,
+		Quantity:           prodQuantity,
+		TemplateData:       templateData}
+	shipmentObject := Shipment{
+		ShipmentSequenceNumber: 1,
+		FirstName:              formData["firstName"][0],
+		LastName:               formData["lastName"][0],
+		CompanyName:            formData["companyName"][0],
+		Address1:               formData["address1"][0],
+		Address2:               formData["address2"][0],
+		City:                   formData["city"][0],
+		State:                  formData["state"][0],
+		PostalCode:             formData["postalCode"][0],
+		CountryCode:            formData["countryCode"][0],
+		Phone:                  formData["phone"][0],
+		ShippingMethod:         "FDXG"}
+	orderObject := CreateOrderObject{
+		PartnerOrderReference: formData["partnerOrderReference"][0],
+		OrderCustomer:         orderCustomer,
+		Items:                 []Item{item},
+		Shipments:             []Shipment{shipmentObject}}
+
+	fmt.Println("partnerOrderRef: " + orderObject.PartnerOrderReference)
+	return orderObject
+}
+
 func fillInTemplatePageHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r.Method)
 	if r.Method == "POST" {
 		r.ParseForm()
-		//body, _ := ioutil.ReadAll(r.Body)
 		fmt.Println(r.Form)
 		fmt.Println("scheme", r.URL.Scheme)
 		fmt.Println(r.Form["url_long"])
@@ -37,14 +100,22 @@ func fillInTemplatePageHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func getStandardOrderFields() string {
+	byteRead, _ := ioutil.ReadFile("orderBasics.txt")
+	customerInfo := string(byteRead)
+
+	return customerInfo
+}
+
 func buildTemplate(productId string) string {
 	var bodyBuffer bytes.Buffer
 	bodyBuffer.WriteString("Please fill out the fields below: \n\n")
 	bodyBuffer.WriteString("<form action=\"processOrder\" method=\"post\">\n")
-	data := getTemplateData(productId)
+	data := getDataFromPFLAPI("products/" + productId)
 	templateList := gjson.Get(data, "results.data.templateFields.fieldlist.field")
 
-	bodyBuffer.WriteString(data)
+	//bodyBuffer.WriteString(data)
+	bodyBuffer.WriteString(getStandardOrderFields())
 
 	fmt.Println("templateList:" + templateList.String())
 	templateList.ForEach(func(key, value gjson.Result) bool {
@@ -55,8 +126,8 @@ func buildTemplate(productId string) string {
 		//defaultValue := value.Get("default").String()
 		//orgValue := value.Get("orgvalue").String()
 
-		bodyBuffer.WriteString("Field: " + fieldName)
-		bodyBuffer.WriteString("<input type=\"test\" name=\"" + fieldName + "\"")
+		bodyBuffer.WriteString("<br>Field: " + fieldName)
+		bodyBuffer.WriteString("<input type=\"text\" name=\"templateField__" + fieldName + "\"")
 		if required {
 			bodyBuffer.WriteString(" required")
 		}
@@ -65,58 +136,43 @@ func buildTemplate(productId string) string {
 		bodyBuffer.WriteString("\n\n")
 		return true
 	})
-	bodyBuffer.WriteString("<input type=\"submit\" value=\"Purchase\">")
+	bodyBuffer.WriteString("<br><br><input type=\"submit\" value=\"Purchase\">")
+	bodyBuffer.WriteString("<input type=\"hidden\" name=\"productID\" value=\"" + productId + "\" />")
+	bodyBuffer.WriteString("</form>")
 	return bodyBuffer.String()
 }
 
-func getTemplateData(productId string) string {
-	req, _ := http.NewRequest("GET", "https://testapi.pfl.com/products/"+productId+"?apikey=136085", nil)
-	req.SetBasicAuth("miniproject", "Pr!nt123")
+func getDataFromPFLAPI(requestLocation string) string {
+	req, _ := http.NewRequest("GET", "https://testapi.pfl.com/"+requestLocation+"?apikey="+apiKey, nil)
+	req.SetBasicAuth(apiUserName, apiPassword)
 	req.Header.Set("Content-Type", "application/json")
 	resp, _ := http.DefaultClient.Do(req)
 	defer resp.Body.Close()
-	fmt.Println("productIDHERe: " + productId)
 	if resp.StatusCode == 200 {
 		bodyBytes, _ := ioutil.ReadAll(resp.Body)
-		body := string(bodyBytes)
-		fmt.Println("here: body is : " + body)
-		return body
+		return string(bodyBytes)
 	}
 	return ""
 }
 
 func showProductListHandler(w http.ResponseWriter, r *http.Request) {
-	req, _ := http.NewRequest("GET", "https://testapi.pfl.com/products?apikey=136085", nil)
-	req.SetBasicAuth("miniproject", "Pr!nt123")
-	req.Header.Set("Content-Type", "application/json")
-	resp, _ := http.DefaultClient.Do(req)
-	defer resp.Body.Close()
-	if resp.StatusCode == 200 {
-		bodyBytes, _ := ioutil.ReadAll(resp.Body)
-		jsonString := string(bodyBytes)
-		productList := gjson.Get(jsonString, "results.data")
-		var bodyBuffer bytes.Buffer
-		bodyBuffer.WriteString("<form action=\"/fillInTemplatePage\" method=\"post\" id=\"productSelection\">\n <input type=\"submit\">\n")
-		bodyBuffer.WriteString("<select name=\"productChoice\">\n")
+	jsonString := getDataFromPFLAPI("products")
+	productList := gjson.Get(jsonString, "results.data")
+	var bodyBuffer bytes.Buffer
+	bodyBuffer.WriteString("<form action=\"/fillInTemplatePage\" method=\"post\" id=\"productSelection\">\n <input type=\"submit\">\n")
+	bodyBuffer.WriteString("<select name=\"productChoice\">\n")
 
-		productList.ForEach(func(key, value gjson.Result) bool {
-			productName := value.Get("name")
-			productId := value.Get("productID")
-			bodyBuffer.WriteString("<option value=\"" + productId.String() + "\">" + productName.String() + "</option>\n")
-			return true
-		})
+	productList.ForEach(func(key, value gjson.Result) bool {
+		productName := value.Get("name")
+		productId := value.Get("productID")
+		bodyBuffer.WriteString("<option value=\"" + productId.String() + "\">" + productName.String() + "</option>\n")
+		return true
+	})
 
-		bodyBuffer.WriteString("</select></form>")
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		fmt.Fprintf(w, bodyBuffer.String())
-	}
+	bodyBuffer.WriteString("</select></form>")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, bodyBuffer.String())
 }
-
-//Would be better to have a way to ty the product ids right to the selection
-// in case of matching names...
-//func getProductIdFromName(fullSelection string, resultName string) int {
-//	return gjson.Get(fullSelection, "results.data.#[name==\""+resultName+"\"].productId")
-//}
 
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Go web app test page 14423")
