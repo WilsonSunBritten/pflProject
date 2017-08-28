0d6545d20e41b0b0eaf7c30eb2a202d3f5d89f6f
Wilson Britten <wilsonsunbritten@gmail.com>
2017-08-27 18:58:05 -0600

    Some cleanup

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 	"strings"
 )
 
+const portValue string = ":23888"
 const apiKey string = "136085"
 const apiUserName string = "miniproject"
 const apiPassword string = "Pr!nt123"
@@ -25,7 +26,7 @@ func main() {
 	http.HandleFunc("/fillInTemplatePage", fillInTemplatePageHandler)
 	http.HandleFunc("/processOrder", processOrderHandler)
 	http.HandleFunc("/testStuff", testStuffHandler)
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(portValue, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 		return
@@ -52,29 +53,11 @@ func processOrderHandler(w http.ResponseWriter, r *http.Request) {
 			prettyOrderJson, _ := json.MarshalIndent(orderObject, "", "	")
 			fmt.Fprintf(w, "Order failed to get a response. Json sent:\n"+string(prettyOrderJson))
 		}
-
 	}
 }
 
 func postDataToPFLAPI(apiPath string, jsonData string) string {
-	pflRequest, err := http.NewRequest("POST", pflAPIBaseLink+apiPath+"?apikey="+apiKey, strings.NewReader(jsonData))
-	if err != nil {
-		//TODO handle error
-	}
-	pflRequest.SetBasicAuth(apiUserName, apiPassword)
-	pflRequest.Header.Set("Content-Type", "application/json")
-
-	pflResponse, err := http.DefaultClient.Do(pflRequest)
-	if err != nil {
-		fmt.Println("error here: " + err.Error())
-		return ""
-	}
-	defer pflResponse.Body.Close()
-	if pflResponse.StatusCode == 200 {
-		bodyBytes, _ := ioutil.ReadAll(pflResponse.Body)
-		return string(bodyBytes)
-	}
-	return ""
+	return pflAPIRequest("POST", apiPath, strings.NewReader(jsonData))
 }
 
 // Returns an object ready for json marshalling for the pfl create order api
@@ -164,11 +147,36 @@ func buildTemplate(productId string) string {
 
 	bodyBuffer.WriteString(getStandardOrderFields())
 	bodyBuffer.WriteString(getShippingChoiceField(data))
+	bodyBuffer.WriteString(getQuantityChoiceField(data))
 	bodyBuffer.WriteString(getTemplateFieldsForPage(data))
 
 	bodyBuffer.WriteString("<br><br><input type=\"submit\" value=\"Purchase\">")
 	bodyBuffer.WriteString("<input type=\"hidden\" name=\"productID\" value=\"" + productId + "\" />")
 	bodyBuffer.WriteString("</form>")
+	return bodyBuffer.String()
+}
+
+func getQuantityChoiceField(jsonData string) string {
+	var bodyBuffer bytes.Buffer
+	defaultQuantity := gjson.Get(jsonData, "results.data.quantityDefault")
+	minimumQuantity := gjson.Get(jsonData, "results.data.quantityMinimum")
+	maximumQuantity := gjson.Get(jsonData, "results.data.quantityMaximum")
+	quantityIncrement := gjson.Get(jsonData, "results.data.quantityIncrement")
+
+	bodyBuffer.WriteString("<br>Quantity: <input type=\"number\" name=\"quantity\"")
+	if len(minimumQuantity.String()) > 0 {
+		bodyBuffer.WriteString(" min=\"" + minimumQuantity.String() + "\"")
+	}
+	if len(maximumQuantity.String()) > 0 {
+		bodyBuffer.WriteString(" max=\"" + maximumQuantity.String() + "\"")
+	}
+	if len(defaultQuantity.String()) > 0 {
+		bodyBuffer.WriteString(" value=\"" + defaultQuantity.String() + "\"")
+	}
+	if len(quantityIncrement.String()) > 0 {
+		bodyBuffer.WriteString(" step=\"" + quantityIncrement.String() + "\"")
+	}
+	bodyBuffer.WriteString(">")
 	return bodyBuffer.String()
 }
 
@@ -179,8 +187,10 @@ func getShippingChoiceField(jsonData string) string {
 	bodyBuffer.WriteString("<br>Shipping Method:<select name=\"shippingMethod\">\n")
 
 	shippingOptionList.ForEach(func(key, value gjson.Result) bool {
-		deliveryMethodCode := value.Get("deliveryMethodCode")
-		bodyBuffer.WriteString("<option value=\"" + deliveryMethodCode.String() + "\">" + deliveryMethodCode.String() + "</option>\n")
+		deliveryMethodCode := value.Get("deliveryMethodCode").String()
+		deliveryMethodDescription := value.Get("description").String()
+		deliveryMethodPrice := value.Get("price").Float()
+		bodyBuffer.WriteString("<option value=\"" + deliveryMethodCode + "\">" + deliveryMethodDescription + "--$" + strconv.FormatFloat(deliveryMethodPrice, 'f', 2, 64) + "</option>\n")
 		return true
 	})
 	bodyBuffer.WriteString("\n</select>\n")
@@ -192,19 +202,24 @@ func getTemplateFieldsForPage(jsonData string) string {
 	templateList := gjson.Get(jsonData, "results.data.templateFields.fieldlist.field")
 
 	if len(templateList.String()) <= 0 {
-		bodyBuffer.WriteString("<br>ItemField: <input type=\"text\" name=\"itemFile\" value=\"http://www.yourdomain.com/files/printReadyArtwork1.pdf\" />")
+		imageURL := gjson.Get(jsonData, "results.data.imageURL").String()
+		if len(imageURL) <= 0 {
+			imageURL = "http://www.yourdomain.com/files/printReadyArtwork1.pdf"
+		}
+		bodyBuffer.WriteString("<br>ItemField: <input type=\"text\" name=\"itemFile\" value=\"" + imageURL + "\" />")
 	}
 	fmt.Println("\n\ntemplateList:" + templateList.String() + "\n\n\n")
 	templateList.ForEach(func(key, value gjson.Result) bool {
+		//TODO utilize additional information
 		fieldName := value.Get("fieldname").String()
 		required := value.Get("required").String() == "Y"
 		//visible := value.Get("visible").String() == "Y"
 		//fieldType := value.Get("type").String()
-		//defaultValue := value.Get("default").String()
+		defaultValue := value.Get("default").String()
 		//orgValue := value.Get("orgvalue").String()
 		//htmlFieldName := value.Get("htmlfieldname").String()
 		bodyBuffer.WriteString("<br>Field: " + fieldName)
-		bodyBuffer.WriteString("<input type=\"text\" name=\"" + templatePrefix + fieldName + "\"")
+		bodyBuffer.WriteString("<input type=\"text\" name=\"" + templatePrefix + fieldName + "\" value=\"" + defaultValue + "\"")
 		if required {
 			bodyBuffer.WriteString(" required")
 		}
@@ -215,13 +230,19 @@ func getTemplateFieldsForPage(jsonData string) string {
 }
 
 func getDataFromPFLAPI(requestLocation string) string {
-	req, _ := http.NewRequest("GET", "https://testapi.pfl.com/"+requestLocation+"?apikey="+apiKey, nil)
-	req.SetBasicAuth(apiUserName, apiPassword)
-	req.Header.Set("Content-Type", "application/json")
-	resp, _ := http.DefaultClient.Do(req)
-	defer resp.Body.Close()
-	if resp.StatusCode == 200 {
-		bodyBytes, _ := ioutil.ReadAll(resp.Body)
+	return pflAPIRequest("GET", requestLocation, strings.NewReader(""))
+}
+
+func pflAPIRequest(requestType string, requestLocation string, jsonData *strings.Reader) string {
+	//TODO handle errors
+	pflRequest, _ := http.NewRequest(requestType, pflAPIBaseLink+requestLocation+"?apikey="+apiKey, jsonData)
+	pflRequest.SetBasicAuth(apiUserName, apiPassword)
+	pflRequest.Header.Set("Content-Type", "application/json")
+
+	pflResponse, _ := http.DefaultClient.Do(pflRequest)
+	defer pflResponse.Body.Close()
+	if pflResponse.StatusCode == 200 {
+		bodyBytes, _ := ioutil.ReadAll(pflResponse.Body)
 		return string(bodyBytes)
 	}
 	return ""
@@ -269,6 +290,5 @@ func testStuffHandler(w http.ResponseWriter, r *http.Request) {
 		bodyBytes, _ := ioutil.ReadAll(resp.Body)
 		bodyString := string(bodyBytes)
 		fmt.Println("am here: ", bodyString)
-
 	}
 }
